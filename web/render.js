569bc612a969dd574202c020ab5c46b541f5115c
Yaroslav Glodov <glodov@gmail.com>
2023-12-27 19:13:34 +0200

    LLM comment removed.

--- web/render.js
+++ web/render.js
@@ -26,8 +26,3 @@ async function renderAllFiles() {
 }
 
 renderAllFiles();
-// @todo To upload changed files on the server I need you to write a server (apache) script that accepts next calls:
-//  - POST postData - list of all files with their relative to rootDir path, size: in the response I receive a list of files with the sizes and 0 if do not exist, for files with the same size server returns their hash. Files which are not present in postData must be returned as separeted list of filesToRemove. Local script check the hashes for the list of files with hashes to understand which must be updated and which not. All the files which no need to be updated must be removed from DIST_DIR. All the rest file are zipped and sending with the next request:
-//  - PUT filesToRemove, dist.zip - unzip the file on the server, removes all the filesToRemove, returns OK on success and ERROR with the proper code and exception on failure.
-// All the api requests must be authorized.
-// All authorized AUTH_KEY and API_URL are stored in .env same on the localhost and server and must be sent through headers.
