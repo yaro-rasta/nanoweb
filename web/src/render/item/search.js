e31bb744802d51dfb26ae688f9cd541bae81aff9
ЯR <glodov@gmail.com>
2024-02-11 04:00:08 +0200

    Update search.js

--- web/src/render/item/search.js
+++ web/src/render/item/search.js
@@ -3,9 +3,11 @@ const path = require('node:path');
 const { runtime } = require('../../runtime');
 const { getLanguage } = require('../../data');
 const { thumbSrc } = require('../../static');
+const { ensureDirectory } = require('../../fs');
 
 const fps = {};
-const blocks = {};
+const blockRowsLimit = runtime['SEARCH_BLOCK_ROWS'] || 0;
+const blockSizeLimit = runtime['SEARCH_BLOCK_SIZE'] || 0;
 
 function close() {
   Object.keys(fps).forEach(file => {
@@ -31,6 +33,10 @@ function fromNano(nano, divider = ' ') {
   return result.join(divider);
 }
 
+function saveMeta() {
+  
+}
+
 function extract(item) {
   let image = '';
   const keys = runtime['SEARCH_IMAGE_KEYS'] || ['ogImage', 'image', 'thumb'];
@@ -53,18 +59,57 @@ function extract(item) {
   ].join("\n");
 }
 
+function blockFile(file, block = 0) {
+  if (!block) return file;
+  return file.replace(/\.txt$/, `-${block}.txt`);
+}
+
+function saveMeta() {
+  const meta = {};
+  for (const file in fps) {
+    const rows = fps[file].rows;
+    const size = fps[file].size;
+    meta[file] = [];
+    for (const i in rows) {
+      meta[file].push({
+        block: blockFile(file, i),
+        rows: rows[i],
+        size: size[i]
+      });
+    }
+  }
+  const file = path.join(runtime['STATIC_DIR'], 'search.json');
+  fs.writeSync(file, JSON.stringify(meta));
+}
+
 function search(item, i, len) {
   const lang = getLanguage(item['$uri']);
   const file = path.join(runtime['STATIC_DIR'], 'search', `${lang}.txt`);
+  ensureDirectory(file);
 
   if (typeof fps[file] === 'undefined') {
-    fps[file] = fs.openSync(file, 'w'); // Open file
+    const fp = fs.openSync(blockFile(file), 'w'); // Open file
+    fps[file] = { fp, rows: [0], size: [0], block: 0 };
   }
   
   const index = extract(item);
-  fs.writeSync(fps[file], index + runtime['SEARCH_INDEX_DIVIDER'] || "\n\n"); // Append newline for each entry
+  const size = Buffer.byteLength(index, 'utf8');
+  const overLong = blockRowsLimit > 0 && rows === blockRowsLimit;
+  const overSize = blockSizeLimit > 0 && size + fps[file].size[fps[file].block] > blockSizeLimit;
+  if (overLong || overSize) {
+    fs.closeSync(fps[file].fp);
+    fps[file].block++;
+    const fp = fs.openSync(blockFile(file, fps[file].block), 'w');
+    fps[file].fp = fp;
+    fps[file].rows[fps[file].block] = 0;
+    fps[file].size[fps[file].block] = 0;
+  }
+  fs.writeSync(fps[file].fp, index + runtime['SEARCH_INDEX_DIVIDER'] || "\n\n"); // Append newline for each entry
+  fps[file].rows[fps[file].block]++;
+  fps[file].size[fps[file].block] += size;
 
   if (len - 1 === i) {
+    saveMeta();
     close(); // Close all file descriptors
   }
 }
